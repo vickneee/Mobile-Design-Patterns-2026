8bd456e5547b0a24c95b392ae96d9e8938aaad00
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-01-23 11:57:44 +0200

    Update commit

--- src/MapExample.kt
+++ src/MapExample.kt
@@ -1,3 +1,5 @@
+import kotlin.math.roundToInt
+
 // Parameter is function
 
 fun mapExample(lista: List<String>, jokuF: (s: String) -> String): List<String> {
@@ -9,7 +11,22 @@ fun mapExample(lista: List<String>, jokuF: (s: String) -> String): List<String> 
     return tulos.toList()
 }
 
+fun arvo(pisteet: Double): Int {
+
+    if (pisteet !in 40.0 .. 100.0) return 0
+
+    val arvosana = 0.5 + (pisteet - 40) * 4.99 / 60
+    return arvosana.roundToInt()
+
+}
+
+fun teeGrade(alaraja: Double) {
+
+}
+
 fun main() {
-    val s = mapExample(listOf("Abba", "Ace Of Base", "Metallica")) { s: String -> s.reversed() }
-    println(s)
+    // val s = mapExample(listOf("Abba", "Ace Of Base", "Metallica")) { s: String -> s.reversed() }
+    // println(s)
+
+    println(arvo(40.0))
 }
