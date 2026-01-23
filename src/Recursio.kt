924716082efec61d5a3dd4b7aa20e7e6a9f63c3e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-01-23 15:33:21 +0200

    Update commit

--- src/Recursio.kt
+++ src/Recursio.kt
@@ -1,3 +1,11 @@
+data class Suoritus(val op: Int, var ar: Int) {
+}
+
+data class Opiskelija(val nro: Int) {
+    val suoritukset = mutableListOf<Suoritus>()
+}
+
+
 
 fun mm(lista: List<String>): List<String> {
     println("Listan pituus: $lista")
@@ -11,4 +19,14 @@ fun mm(lista: List<String>): List<String> {
 
 fun main () {
     println(mm(listOf("a","ab","abc")))
+
+    val o = (1 .. 5).map { Opiskelija(it) }
+    println(o)
+    (1 .. 5).forEach {
+        o.forEach { it.suoritukset.add(Suoritus((1..5).random(), (1..5).random())) }
+    }
+    o.forEach { println(it.suoritukset) }
+    
+    println(o.map { it.suoritukset.size }.sum())
+    o.forEach { it -> it.suoritukset.forEach {it.op} }
 }
