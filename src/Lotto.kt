b3925c89dc071200b543d6630d80c7613752cd37
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-01-28 13:09:17 +0200

    Update commit

--- src/Lotto.kt
+++ src/Lotto.kt
@@ -1,5 +1,3 @@
-import kotlin.uuid.Uuid.Companion.random
-
 fun arvo(n: Int, alue: IntRange): List<Int>? {
     if (n < 0 || alue.count() < n) return null
 
@@ -14,25 +12,31 @@ fun arvo(n: Int, alue: IntRange): List<Int>? {
     return alue.shuffled().take(n).sorted()
 }
 
-fun laillinen(rivi: List<Int>, n: Int, alue: IntRange) = rivi.all { it in alue } && rivi.toSet().size == n
+fun laillinen(rivi: List<Int>?, n: Int, alue: IntRange): Boolean {
+    if (rivi == null) return false
+    return rivi.all { it in alue } && rivi.toSet().size == n
+}
 
-fun tarkista(secret: List<Int>, guess: List<Int>): Int? {
+fun tarkista(secret: List<Int>, guess: List<Int>?): Int? {
     if (!laillinen(rivi = guess, n = 7, 1 .. 40)) return null
 
-    return secret.intersect(guess.toSet()).size
+    return secret.toSet().intersect(guess?.toSet() ?: setOf()).size
 }
 
 fun main() {
 
-    println(arvo(7, 1 .. 20))
-
-    val secret = (0..40).shuffled().take(7)
+    val secret = (1..40).shuffled().take(7)
 
     println("Arvottu rivi: $secret")
-    var n = 0
-    while (true) {
-        val guess = (1 .. 40).shuffled().take(7)
-        n++
-        guess.intersect(guess.toSet())
+
+    val guess = arvo(7, 1..40)   // your chosen numbers
+    println("Oma rivi: $guess")
+
+    val result = tarkista(secret, guess)
+
+    if (result != null) {
+        println("Oikeita numeroita: $result")
+    } else {
+        println("Virheellinen rivi")
     }
 }
