53a767fbf1f42f61d1e6fdf12dd2de90d8149571
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-01-30 10:49:13 +0200

    Update commit

--- src/Student.kt
+++ src/Student.kt
@@ -1,11 +1,17 @@
 class Student {
-    var grades = listOf<Int>()
-        private set
+//    var grades = listOf<Int>()
+//        private set
+
+    private val _grades = mutableListOf<Int>()
+    // Computed property
+    val grades: List<Int>  // Read-only public property
+        get() = _grades.toList() // Immutable list
+
     fun addGrade(g: Int) {
         // grades.add(100) not possible if grades is val and immutable list
 
         if (g in 1..5) {
-            grades += g
+            _grades += g
         } else {
             println("Invalid grade: $g")
         }
