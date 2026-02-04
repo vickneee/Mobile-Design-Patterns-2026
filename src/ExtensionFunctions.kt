ff56bfca719d76807794f165c8eb227de77fe5bf
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-04 21:24:41 +0200

    Update commit

--- src/ExtensionFunctions.kt
+++ src/ExtensionFunctions.kt
@@ -1,2 +1,13 @@
-class ExtensionFunctions {
+
+fun String.neeExtensionFunction(): String {
+    val changeCaseTest: String = this.filter { it.isUpperCase() }
+    return changeCaseTest
+}
+
+fun extensionExample(string: String): String {
+    return string.neeExtensionFunction()
+}
+
+fun main() {
+    extensionExample("hello")
 }
