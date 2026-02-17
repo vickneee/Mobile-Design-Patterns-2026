02929f31caf9028b09df75d9566f3bb2a8cacc64
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-18 01:55:03 +0200

    Update commit

--- src/Basics.kt
+++ src/Basics.kt
@@ -38,6 +38,30 @@ fun main() {
     println("$firstNumber + $thirdNumber = $anotherResult")
     println("$firstNumber - $thirdNumber = $subtractResult")
     println("$firstNumber - $thirdNumber = $anotherSubtractResult")
+
+    // Default parameters
+    val operatingSystem = "Chrome OS"
+    val emailId = "sample@gmail.com"
+
+    println(displayAlertMessage(operatingSystem, emailId))
+    println()
+
+    val firstUserEmailId = "user_one@gmail.com"
+
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
 }
 
 fun add(first: Int, second: Int): Int {
@@ -46,4 +70,8 @@ fun add(first: Int, second: Int): Int {
 
 fun subtract(first: Int, second: Int): Int {
     return first - second
+}
+
+fun displayAlertMessage(os: String = "Unknown OS", emailId: String = "sample@gmail.com"): String {
+    return "There's a new sign-in request on $os for your Google Account $emailId."
 }
